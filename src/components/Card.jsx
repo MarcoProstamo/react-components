cced00690a5eda580e59e94f17b1793d94cb6d63
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-28 14:20:33 +0100

    Add → Header

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import Button from "./Button/Button";
+
+export default function Card() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1 className="text-center">Il mio Blog</h1>
+    </>
+  );
+}
