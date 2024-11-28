cfa7a74e736a7a917b067fbfba655a5566bea860
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-28 14:25:20 +0100

    Add → Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <>
-      <h1 className="text-center">Il mio Blog</h1>
+      <header>
+        <h1 className="text-center">Il mio Blog</h1>
+      </header>
     </>
   );
 }
